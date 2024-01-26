8b7f38ed77468282d583e64f5b787dfb1588ebcc
Athul Raj <mail@athulraj.com>
2024-01-26 17:13:26 +0530

    v0.2.4

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
-import { Church, Member } from "./Services/"
+import { UUID } from "crypto"
+import Church from "./Services/Church"
+import Member from "./Services/Member"
 
-export default {
+export {
     Church,
     Member,
+}
+
+export default {
+    ChurchClient: () => new Church(),
+    MemberClient: (tenantBaseURL: string, version_uuid: UUID, sessionToken: UUID) => new Member(tenantBaseURL, version_uuid, sessionToken) => new Member(tenantBaseURL, version_uuid, sessionToken),
 }
